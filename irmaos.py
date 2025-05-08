0aaa1d888eb0f9da5f79aa2ad7e5b9efaeba6ceb
S. Silva Domingos <suyannegomes01@gmail.com>
2025-05-08 16:07:57 -0300

    Add files via upload

---
+++ irmaos.py
@@ -0,0 +1,11 @@
+#otavio - Oscar = diferenca
+#Orlando = otavio + diferemca
+
+N = int(input())
+M = int(input())
+if N > M:
+    print("O Oscar é mais novo")
+else:
+    diferenca = M - N
+    Orlando = diferenca + M
+    print(Orlando)
